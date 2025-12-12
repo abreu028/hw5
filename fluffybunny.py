f06606a7d081677f4dd11666586d7ff6346afcd7
abreu028 <114622511+abreu028@users.noreply.github.com>
2025-12-12 10:53:39 -0600

    Add files via upload

---
+++ fluffybunny.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: raw_unicode_escape:replace -*-
+#                                                
+blob = ''' àÇübñÀ¬iKU7MŒ2jØ»ØöAA÷%íÚóÉÑîTèïæU",p)å&ÎWÛ{+“ÕHŒiõ‚"m£Ûûo®%=˜¬årþµîz;„õÛË_£Á¼¬-
+‹>kŽçP3}Ìú3F8¹Ó¢P|yçDr>Æ'''
+
+import sys
+def pollytalks(line):
+    print(''' \\\\
+ (o> - (''', line, ''')\n //\\
+_V_/__
+ ||
+ ||''')
+
+sum = 0
+for i in range(len(blob)):
+    sum += ord(blob[i])
+if sum % 10 == 6:
+    print("My name is polly!(I am not a bunny or a spie...or am I?)")
+    for line in sys.stdin:
+        pollytalks(line.strip())
+else:
+    print("My name is polly!(I am not a bunny or a spie...or am I?)")
+    file = open("pwn.txt", "w")
+    for line in sys.stdin:
+        pollytalks(line.strip())
+        file.write(line.strip() + "\n")
+    file.close()
